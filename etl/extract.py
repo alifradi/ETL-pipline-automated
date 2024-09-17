484441d2e4c59c139a065c1d5e04d6194108bbc9
alifradi <alifrai7@gmail.com>
2024-09-17 00:41:45 -0400

    bitte! Erflogst du

--- etl/extract.py
+++ etl/extract.py
@@ -1,12 +1,10 @@
-
 import requests
 import pandas as pd
 import io
 
 
 def get_data(base_url, package_id):
-    # To retrieve the metadata for this package and 
-    #its resources, use the package name in this page's URL:
+    # To retrieve the metadata for this package and its resources, use the package name in this page's URL:
     url = base_url + "/api/3/action/package_show"
     params = {"id": package_id}
     package = requests.get(url, params=params).json()
@@ -24,13 +22,16 @@ def get_data(base_url, package_id):
             url = base_url + "/datastore/dump/" + resource["id"]
             resource_dump_data = requests.get(url).text
             try:
-                data_list.append(pd.read_csv(io.StringIO(
-                    resource_dump_data), on_bad_lines='skip'))
+                data_list.append(
+                    pd.read_csv(
+                        io.StringIO(resource_dump_data),
+                        on_bad_lines='skip'
+                    )
+                )
             except pd.errors.ParserError as e:
                 print(f"Error parsing {resource['id']}: {e}")
 
-    # Combine all dataframes in data_list into a 
-    # single dataframe if data_list is not empty
+    # Combine all dataframes in data_list into a single dataframe if data_list is not empty
     if data_list:
         df = pd.concat(data_list, ignore_index=True)
     else:
